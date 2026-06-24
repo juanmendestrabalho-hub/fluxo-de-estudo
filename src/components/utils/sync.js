c943395572fd447a4f3ea5565c082451881df972
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-06-23 21:20:30 -0300

    Update sync.js

--- src/components/utils/sync.js
+++ src/components/utils/sync.js
@@ -1,1 +1,6 @@
+const channel = new BroadcastChannel("studyflow");
 
+export const syncSend = (data) => channel.postMessage(data);
+
+export const syncListen = (callback) =>
+  channel.onmessage = (e) => callback(e.data);
