0d7f5c3a0f14d737e38246257f13c69960e4103b
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-06-23 21:18:09 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        glass: "rgba(255,255,255,0.08)"
+      }
+    }
+  },
+  plugins: []
+}
